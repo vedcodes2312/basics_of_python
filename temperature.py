c549d176a9ec27b8690202ebe7e3d2c29d4cc3be
Ved Vyas <vedvyascodes1608@gmail.com>
2024-10-21 21:02:36 +0530

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,9 @@
+#celsius to fahrenheit
+
+celsius=float(input("enter temperature in celcius: "))
+#user will imput temperature in  celsius
+
+fahrenheit = ((celsius*9)/5)+32
+#required conversion
+
+print("temperature in celsius",celsius,"is equal to",fahrenheit,"degree fahrenheit")
