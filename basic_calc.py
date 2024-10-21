7a48faf773e79268da903bf47808b2adc8d0a24e
Ved Vyas <vedvyascodes1608@gmail.com>
2024-10-21 20:53:25 +0530

    Add files via upload

---
+++ basic_calc.py
@@ -0,0 +1,26 @@
+#basic calculator in python
+a=float(input("enter first number: "))
+b=float(input("enter second number: "))
+
+sum=a+b
+difference=a-b
+product=a*b
+quotient=a/b
+remainder=a%b
+
+print("the sum of a=",a,"and b=",b, "is ", sum)
+print(a,"+",b,"=",sum)
+
+print("the difference of a=",a,"and b=",b, "is ", difference)
+print(a,"-",b,"=",difference)
+
+print("the product of a=",a,"and b=",b, "is ", product)
+print(a,"*",b,"=",product)
+
+print("the quotient of a=",a,"and b=",b, "is ", quotient)
+print(a,"/",b,"=",quotient)
+
+print("the remainder of a=",a,"and b=",b, "is ", remainder)
+print(a,"%",b,"=",remainder)
+
+
