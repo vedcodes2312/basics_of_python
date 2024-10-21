e0cea4edf30b4493b7af7ab40edf9130663475e1
Ved Vyas <vedvyascodes1608@gmail.com>
2024-10-21 11:47:28 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,7 @@
+print("hello world")
+countryname="INDIA"
+capital="NEW DELHI"
+independence="1947"
+print("my country" ,countryname)
+print("capital of my country",capital)
+print("got independence in the year",independence)
